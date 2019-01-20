280fd32ebba2684ab6771a1c285cbc85fe79d85c
JaniceNova <janicenovacek2016@gmail.com>
2019-01-19 21:23:08 -0800

    changes

--- server.js
+++ server.js
@@ -34,21 +34,21 @@ mongoose.connect("mongodb://localhost/mongooseHeadlines", { useNewUrlParser: tru
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.nytimes.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
+        .find("span.balancedHeadline")
         .text();
       result.link = $(this)
-        .children("a")
+        .find("a")
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
@@ -122,3 +122,7 @@ app.post("/articles/:id", function(req, res) {
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
+
+
+
+// http://www.echojs.com/
